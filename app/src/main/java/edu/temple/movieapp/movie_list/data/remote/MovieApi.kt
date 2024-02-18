15936eb4390971efd5ab462437f6f9e3e7003eb7
jaime Nguyen <nguyenphuocdat168@gmail.com>
2024-02-18 10:26:56 -0500

    Build the Api data

--- app/src/main/java/edu/temple/movieapp/movie_list/data/remote/MovieApi.kt
+++ app/src/main/java/edu/temple/movieapp/movie_list/data/remote/MovieApi.kt
@@ -1,4 +1,21 @@
 package edu.temple.movieapp.movie_list.data.remote
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface MovieApi {
+
+    @GET("movie/{category}")
+    suspend fun getMovieList(
+        @Path("category") category: String,
+        @Query("page") page: Int,
+        @Query("api_key") apiKey: String = API_KEY
+    )
+
+    companion object {
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+        const val API_KEY = "4c86cf78c31d657c3f0b4d561e83839a"
+    }
 }
---
+++ app/src/main/java/edu/temple/movieapp/movie_list/data/remote/response/MovieListDto.kt
@@ -0,0 +1,8 @@
+package edu.temple.movieapp.movie_list.data.remote.response
+
+data class MovieListDto(
+    val page: Int,
+    val results: List<MovieDto>,
+    val total_pages: Int,
+    val total_results: Int
+)
